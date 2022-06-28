760147457515c3836639423ff0f1e734124060f4
McDonald, Patrick <patrick.mcdonald@fmr.com>
2022-06-28 14:21:56 -0400

    Add segment and tall/short list
    This breaks like the current issue

--- src/app/segments-that-change-height/segments-that-change-height.component.ts
+++ src/app/segments-that-change-height/segments-that-change-height.component.ts
@@ -1,14 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-segments-that-change-height',
   templateUrl: './segments-that-change-height.component.html',
   styleUrls: ['./segments-that-change-height.component.scss'],
 })
-export class SegmentsThatChangeHeightComponent implements OnInit {
+export class SegmentsThatChangeHeightComponent implements AfterViewInit {
 
-  constructor() { }
+  numOfItems = 15;
 
-  ngOnInit() {}
+  constructor(
+    private el: ElementRef
+  ) { }
+
+  async ngAfterViewInit() {
+    await document.querySelector('ion-app').componentOnReady();
+    console.log('after', this.el);
+    console.log('after', this.el.nativeElement.offsetHeight);
+  }
+
+  segmentChanged(event: Event) {
+    const {
+      detail: { value }
+    } = event as any;
+    console.log(value);
+    this.numOfItems = value;
+  }
 
 }
