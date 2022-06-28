c1926e5968eeaddc96fb7c21e71f0d3da67f4dfe
McDonald, Patrick <patrick.mcdonald@fmr.com>
2022-06-28 16:43:27 -0400

    Set min height when changing segments

--- src/app/segments-that-change-height/segments-that-change-height.component.ts
+++ src/app/segments-that-change-height/segments-that-change-height.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef } from '@angular/core';
+import { AfterViewInit, Component, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-segments-that-change-height',
@@ -7,30 +7,60 @@ import { AfterViewInit, Component, ElementRef } from '@angular/core';
 })
 export class SegmentsThatChangeHeightComponent implements AfterViewInit {
 
+  @ViewChild('outerCard') outerCard: any;
+  outerCardMinHeight = '0px';
+
   numOfItems = 15;
 
   constructor(
-    private el: ElementRef
+    private renderer: Renderer2
   ) { }
 
   async ngAfterViewInit() {
+    /**
+     * Make sure Ionic is ready to get the height;
+     * https://github.com/ionic-team/ionic-framework/issues/17920#issuecomment-478997010
+     */
     await document.querySelector('ion-app').componentOnReady();
-    console.log('after', this.el);
-    console.log('after', this.el.nativeElement.offsetHeight);
+    // console.log(this.outerCard);
+    // console.log(this.outerCard.el.offsetHeight);
+    /**
+     * Comment this out and we'll see the accidental click the first time.
+     */
+    this.setOuterCardMinHeight();
   }
 
   segmentChanged(event: Event) {
     const {
       detail: { value }
     } = event as any;
-    // console.log(value);
     this.numOfItems = value;
 
-    console.log('BEFORE timeout', this.el.nativeElement.offsetHeight);
+    // console.log('BEFORE timeout', this.outerCard.el.offsetHeight);
     setTimeout(() => {
-      console.log('AFTER timeout', this.el.nativeElement.offsetHeight);
+      // console.log('AFTER timeout', this.outerCard.el.offsetHeight);
+      this.setOuterCardMinHeight();
     });
+  }
 
+  setOuterCardMinHeight() {
+    /**
+     * We set the card to the greater of it's last height
+     * or the new height. We are keeping track of it with
+     * outerCardMinHeight and then also setting the style.
+     */
+    this.outerCardMinHeight = `${
+      Math.max(
+        parseFloat(this.outerCardMinHeight),
+        this.outerCard.el.offsetHeight
+      )
+    }px`;
+
+    this.renderer.setStyle(
+      this.outerCard.el,
+      'min-height',
+      this.outerCardMinHeight
+    );
   }
 
 }
